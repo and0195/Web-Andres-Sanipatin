96818f5918891faef7992188a2e8832dd5cf63bd
Andres <casanipatin@gmail.com>
2021-04-08 11:48:32 -0500

    “prueba”

---
+++ Subpag/mainProye.js
@@ -0,0 +1,11 @@
+document.querySelector('.menu-btn').addEventListener('click',() =>{
+    document.querySelector('.nav-menu').classList.toggle('show');
+})
+
+ScrollReveal().reveal('.showcase', {delay:500});
+ScrollReveal().reveal('.cards-banner-one', {delay:500});
+ScrollReveal().reveal('.cards-banner-one1', {delay:500});
+ScrollReveal().reveal('.cards-banner-one2', {delay:500});
+ScrollReveal().reveal('.cards-banner-one3', {delay:500});
+ScrollReveal().reveal('.cards-banner-one4', {delay:500});
+ScrollReveal().reveal('.cards-banner-one6', {delay:500});
---
+++ main.js
@@ -0,0 +1,12 @@
+document.querySelector('.menu-btn').addEventListener('click',() =>{
+    document.querySelector('.nav-menu').classList.toggle('show');
+})
+
+ScrollReveal().reveal('.showcase', {delay:500});
+ScrollReveal().reveal('.cards-banner-one', {delay:500});
+ScrollReveal().reveal('.cards-banner-one1', {delay:500});
+ScrollReveal().reveal('.cards-banner-one2', {delay:500});
+ScrollReveal().reveal('.cards-banner-one3', {delay:500});
+
+
+
